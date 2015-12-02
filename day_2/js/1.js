8b9d9760aaa7688026a90cfee00986db0bef7129
James Monger <jameskmonger@hotmail.co.uk>
2015-12-02 12:39:33 +0000

    JS solution for day 2

---
+++ day_2/js/1.js
@@ -0,0 +1,29 @@
+var input = `2x3x4`;
+
+Array.min = function( array ){
+  return array.reduce(function(a, b, i, array) {return Math.min(a,b)});
+};
+
+var total_paper = 0;
+
+var lines = input.split('\n');
+
+for (var line of lines) {
+  var dimensions = line.split('x');
+  
+  var length = parseInt(dimensions[0]);
+  var width = parseInt(dimensions[1]);
+  var height = parseInt(dimensions[2]);
+  
+  var sides = [];
+  
+  sides[1] = (length * width);
+  sides[2] = (width * height);
+  sides[3] = (height * length);
+  
+  var box_paper = (2 * sides[1]) + (2 * sides[2]) + (2 * sides[3]) + Array.min(sides);
+  
+  total_paper += box_paper;
+}
+
+console.log(total_paper);
