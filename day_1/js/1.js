313cd612177e5ad5132c703e5753cce6bfb8e749
James Monger <jameskmonger@hotmail.co.uk>
2015-12-02 12:35:14 +0000

    JS solution for day 1 part 1

---
+++ day_1/js/1.js
@@ -0,0 +1,15 @@
+var input = "))(((((";
+
+var characterCount = [];
+
+for (var i = 0; i < input.length; i++) {
+    if (characterCount[input[i]] === undefined) {
+    	characterCount[input[i]] = 0;   
+    }
+    
+	characterCount[input[i]]++;
+}
+
+var current_floor = characterCount['('] - characterCount[')'];
+
+console.log(current_floor);
