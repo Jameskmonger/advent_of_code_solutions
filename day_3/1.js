a86890504854ad849a1bf4e2f56f807540bbb844
James Monger <jameskmonger@hotmail.co.uk>
2015-12-03 09:01:49 +0000

    Start parsing direction list

--- day_3/1.js
+++ day_3/1.js
@@ -22,14 +22,33 @@ class DeliveryManager {
 		this.houses = [];
 		
 		this.directions = this.parseDirections(input);
+		
+		this.location = [0, 0];
+		
+		for (var d of this.directions) {
+			if (d === Directions.NORTH) {
+				console.log("up");
+			}
+			if (d === Directions.EAST) {
+				console.log("right");
+			}
+			if (d === Directions.SOUTH) {
+				console.log("down");
+			}
+			if (d === Directions.WEST) {
+				console.log("left");
+			}
+		}
 	}
 	
 	parseDirections(input) {
 		var output = [];
 		
 		for (var i = 0; i < input.length; i++) {
-			
+			output.push(input[i]);
 		}
+		
+		return output;
 	}
 }
 
