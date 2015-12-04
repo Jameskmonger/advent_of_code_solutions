724d4310fb1c71cbf8fbd903f4be606186dacdbf
James Monger <jameskmonger@hotmail.co.uk>
2015-12-04 09:27:30 +0000

    Use while rather than for in d4/p2

--- day_4/js/1.js
+++ day_4/js/1.js
@@ -1,10 +1,13 @@
 var md5 = require('./md5').md5;
 
-const SECRET_KEY = 'yzbqklnj';
+const SECRET_KEY = '';
 
-for (var i = 0; i < 1000000; i++) {
+var i = 0;
+while (true) {
 	if (md5(SECRET_KEY + i).startsWith('00000')) {
 		console.log(i);
 		break;
 	}
+	
+	i++;
 }
