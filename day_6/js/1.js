d3b602706a3cf888e322863f6278d8da19bbd796
James Monger <jameskmonger@hotmail.co.uk>
2015-12-06 12:04:00 +0000

    Started work on day 6 part 1

---
+++ day_6/js/1.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var SIZE = 1000;
+
+var State = {
+  OFF: 0,
+  ON: 1
+}
+
+class Light {
+  constructor() {
+    this.state = State.ON;
+  }
+}
+
+function setupLights() {
+  var lights = [];
+  for (var i = 0; i < SIZE; i++) {
+    lights[i] = [];
+
+    for (var l = 0; l < SIZE; l++) {
+      lights[i][l] = new Light();
+    }
+  }
+}
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+var InstructionType = {
+  OFF: 0,
+  ON: 1,
+  TOGGLE: 2
+}
+
+class Instruction {
+  constructor(type, start, end) {
+    this.type = type;
+    this.start = start;
+    this.end = end;
+  }
+}
+
+function parseInstructions(input) {
+  for (var line of input.split('\n')) {
+    var type = undefined;
+
+    if (line.startsWith("turn off ")) {
+      type = InstructionType.OFF;
+    }
+
+    if (line.startsWith("turn on ")) {
+      type = InstructionType.ON;
+    }
+
+    if (line.startsWith("toggle ")) {
+      type = InstructionType.TOGGLE;
+    }
+
+    console.log(type);
+  }
+}
+
+setupLights();
+
+var input = `turn off 199,133 through 461,193
+toggle 322,558 through 977,958
+turn on 226,196 through 599,390`;
+
+parseInstructions(input);
