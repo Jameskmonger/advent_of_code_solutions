b93901d311cf07dc09e87f469bcf7e69d28a0426
James Monger <jameskmonger@hotmail.co.uk>
2015-12-07 13:17:18 +0000

    Parse each instruction line

--- day_6/js/1.js
+++ day_6/js/1.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const GRID_SIZE = 1000;
 
 var lights = [];
@@ -7,3 +9,21 @@ for (var i = 0; i < GRID_SIZE; i++) {
 }
 
 var exp = /^(\bturn off \b|\btoggle \b|\bturn on \b)(\d+),(\d+)(\b through \b)(\d+),(\d+)/gm;
+
+var input = ``;
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+for (var line of input.split('\n')) {
+  var parsed = exp.exec(input);
+
+  var instruction = parsed[1];
+
+  var start = new Point(parsed[2], parsed[3]);
+  var end = new Point(parsed[5], parsed[6]);
+}
