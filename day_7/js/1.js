8a711d5c8e649d65ecfb22562e853302542ee41e
James Monger <jameskmonger@hotmail.co.uk>
2015-12-07 12:29:28 +0000

    Remove Array.prototype.remove!

--- day_7/js/1.js
+++ day_7/js/1.js
@@ -1,16 +1,5 @@
 "use strict";
 
-Array.prototype.remove = function() {
-    var what, a = arguments, L = a.length, ax;
-    while (L && this.length) {
-        what = a[--L];
-        while ((ax = this.indexOf(what)) !== -1) {
-            this.splice(ax, 1);
-        }
-    }
-    return this;
-};
-
 var input = "";
 
 var lines = input.split('\n');
