e19e534f49504be9805c3ab8e5a598696f710baf
James Monger <jameskmonger@hotmail.co.uk>
2015-12-09 12:38:17 +0000

    Store all distances and locations

--- day_9/js/1.js
+++ day_9/js/1.js
@@ -1,6 +1,46 @@
-var input = `Dublin -> London -> Belfast = 982
-London -> Dublin -> Belfast = 605
-London -> Belfast -> Dublin = 659
-Dublin -> Belfast -> London = 659
-Belfast -> Dublin -> London = 605
-Belfast -> London -> Dublin = 982`;
+var input = `London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141`;
+
+var lines = input.split('\n');
+
+var path = [],
+    locations = [];
+
+function setDistance(a, b, distance) {
+  if (path[a] === undefined) {
+    path[a] = [];
+  }
+
+  if (path[b] === undefined) {
+    path[b] = [];
+  }
+
+  path[a][b] = parseInt(distance);
+  path[b][a] = parseInt(distance);
+}
+
+function registerLocation(loc) {
+  if (locations.indexOf(loc) === -1) {
+    locations.push(loc);
+  }
+}
+
+for (var line of lines) {
+  var EXP = /^([a-zA-Z]+)\b to \b([a-zA-Z]+)\b = \b(\d+)$/g;
+
+  var parsed = EXP.exec(line);
+
+  var a = parsed[1],
+      b = parsed[2],
+      dist = parsed[3];
+
+  registerLocation(a);
+  registerLocation(b);
+
+  setDistance(a, b, dist);
+}
+
+for (var loc of locations) {
+  console.log(loc);
+}
