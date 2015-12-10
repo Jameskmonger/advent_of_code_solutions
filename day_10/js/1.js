b76d34d271eefc3459c614d48dc2215008a0f1f7
James Monger <jameskmonger@hotmail.co.uk>
2015-12-10 09:05:22 +0000

    Split the string into parts

--- day_10/js/1.js
+++ day_10/js/1.js
@@ -5,7 +5,8 @@ function getSplitIndexes(str) {
   // Initialise the last checked character as the first in the string
   var last_char = str[0];
 
-  var splits = [];
+  // We need to include the first segment
+  var splits = [0];
   for (var i in str) {
     // If this char is different to the last one we checked, add it to the list
     if (str[i] !== last_char) {
@@ -18,11 +19,25 @@ function getSplitIndexes(str) {
   return splits;
 }
 
-function process(str) {
+function getParts(str) {
   var splits = getSplitIndexes(str);
 
-  console.log(splits);
+  var parts = [];
+
+  for (var i = 0; i < splits.length; i++) {
+    var now = splits[i];
+    var next = splits[i + 1];
+
+    if (next === undefined) {
+      parts.push(str.substring(now));
+    } else {
+      parts.push(str.substring(now, next));
+    }
+  }
 }
 
-process("121");
-process("1112215");
+function process(str) {
+  var parts = getParts(str);
+}
+
+process("111223333");
