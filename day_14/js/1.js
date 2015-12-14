c141b63b596b2f4b38cccfcfaa9b735799b48e1a
James Monger <jameskmonger@hotmail.co.uk>
2015-12-14 11:58:45 +0000

    Finish solution

--- day_14/js/1.js
+++ day_14/js/1.js
@@ -12,9 +12,43 @@ class Reindeer {
 
     this.state = STATE_FLYING;
     this.time = this.stamina;
+    this.distance = 0;
+  }
+
+  move() {
+    if (this.state === STATE_FLYING) {
+      this.distance += this.speed;
+      this.time -= 1;
+
+      if (this.time === 0) {
+        this.state = STATE_RESTING;
+        this.time = this.rest;
+        return;
+      }
+    } else if (this.state === STATE_RESTING) {
+      this.time -= 1;
+
+      if (this.time === 0) {
+        this.state = STATE_FLYING;
+        this.time = this.stamina;
+        return;
+      }
+    }
   }
 }
 
 var reindeer = [];
 reindeer.push(new Reindeer("Comet", 14, 10, 127));
 reindeer.push(new Reindeer("Dancer", 16, 11, 162));
+
+const RACE_TIME = 1000;
+
+for (var i = 0; i < RACE_TIME; i++) {
+  for (var r of reindeer) {
+    r.move();
+  }
+}
+
+for (var r of reindeer) {
+  console.log(r.name + ": " + r.distance);
+}
