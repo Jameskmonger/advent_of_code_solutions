47deec3cf52752a1879393990d97af02ae099e73
James Monger <jameskmonger@hotmail.co.uk>
2015-12-15 09:22:46 +0000

    Load ingredients into memory

--- day_15/js/1.js
+++ day_15/js/1.js
@@ -2,11 +2,11 @@
 
 class Ingredient {
   constructor(capacity, durability, flavor, texture, calories) {
-    this.capacity = capacity;
-    this.durability = durability;
-    this.flavor = flavor;
-    this.texture = texture;
-    this.calories = calories;
+    this.capacity = parseInt(capacity);
+    this.durability = parseInt(durability);
+    this.flavor = parseInt(flavor);
+    this.texture = parseInt(texture);
+    this.calories = parseInt(calories);
   }
 }
 
@@ -15,7 +15,10 @@ Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3`.split('\n
 
 var ingredients = {};
 
-for (var line in lines) {
-  var EXP = /([a-zA-Z]+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)/g;
-  
+for (var line of lines) {
+  var parsed = /([a-zA-Z]+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)/g.exec(line);
+
+  ingredients[parsed[1]] = new Ingredient(parsed[2], parsed[3], parsed[4], parsed[5], parsed[6]);
 }
+
+console.log(ingredients);
