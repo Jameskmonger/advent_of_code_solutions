a7cc6802bc8fab223a84969db0335ba024bd5970
James Monger <jameskmonger@hotmail.co.uk>
2015-12-16 09:29:45 +0000

    Parse details from each line

--- day_16/js/1.js
+++ day_16/js/1.js
@@ -1,6 +1,6 @@
-var lines = `Sue 1: children 1, samoyeds 2, trees 3
-Sue 2: children 3, cats 7, perfumes 1
-Sue 3: samoyeds 2, trees 3, akitas 2`.split('\n');
+var lines = `Sue 1: children: 1, samoyeds: 2, trees: 3
+Sue 2: children: 3, cats: 7, perfumes: 1
+Sue 3: samoyeds: 2, trees: 3, akitas: 2`.split('\n');
 
 var requirements = {
   children: 3,
@@ -14,3 +14,14 @@ var requirements = {
   cars: 2,
   perfumes: 1
 };
+
+for (var line of lines) {
+  var parsed = /Sue (\d+): ([a-z]+): (\d+), ([a-z]+): (\d+), ([a-z]+): (\d+)/.exec(line);
+
+  var details = [
+    [ parsed[2], parseInt(parsed[3]) ],
+    [ parsed[4], parseInt(parsed[5]) ],
+    [ parsed[6], parseInt(parsed[7]) ]
+  ];
+
+}
