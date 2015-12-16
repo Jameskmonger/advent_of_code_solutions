e45bd99dbc785fb601414957b96484b3bba2b4cd
James Monger <jameskmonger@hotmail.co.uk>
2015-12-16 09:23:06 +0000

    Add test data

---
+++ day_16/js/1.js
@@ -0,0 +1,16 @@
+var lines = `Sue 1: children 1, samoyeds 2, trees 3
+Sue 2: children 3, cats 7, perfumes 1
+Sue 3: samoyeds 2, trees 3, akitas 2`.split('\n');
+
+var requirements = {
+  children: 3,
+  cats: 7,
+  samoyeds: 2,
+  pomeranians: 3,
+  akitas: 0,
+  vizslas: 0,
+  goldfish: 5,
+  trees: 3,
+  cars: 2,
+  perfumes: 1
+};
