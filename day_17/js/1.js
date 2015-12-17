c272fc9ff8f5bda6ec3001bd65fb086662633ddf
James Monger <jameskmonger@hotmail.co.uk>
2015-12-17 23:22:50 +0000

    Back to the start

--- day_17/js/1.js
+++ day_17/js/1.js
@@ -1,30 +1,9 @@
-var lines = `20
+"use strict";
+
+var sizes = `20
 15
 10
 5
-5`.split('\n');
+5`.split('\n').map(s => parseInt(s));
 
-function permutator(inputArr) {
-  var results = [];
-
-  function permute(arr, memo) {
-    var cur;
-
-    if (memo === undefined) {
-      memo = [];
-    }
-
-    for (var i = 0; i < arr.length; i++) {
-      cur = arr.splice(i, 1);
-      if (arr.length === 0) {
-        results.push(memo.concat(cur));
-      }
-      permute(arr.slice(), memo.concat(cur));
-      arr.splice(i, 0, cur[0]);
-    }
-
-    return results;
-  }
-
-  return permute(inputArr);
-}
+const REQUIRED_AMOUNT = 25;
