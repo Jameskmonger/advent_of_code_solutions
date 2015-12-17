79fc8e5a3f489fb4623ace7df0911162088490e9
James Monger <jameskmonger@hotmail.co.uk>
2015-12-17 23:07:04 +0000

    Add permutator function

--- day_17/js/1.js
+++ day_17/js/1.js
@@ -3,3 +3,28 @@ var lines = `20
 10
 5
 5`.split('\n');
+
+function permutator(inputArr) {
+  var results = [];
+
+  function permute(arr, memo) {
+    var cur;
+
+    if (memo === undefined) {
+      memo = [];
+    }
+
+    for (var i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+
+    return results;
+  }
+
+  return permute(inputArr);
+}
