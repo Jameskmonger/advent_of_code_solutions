2da04f9f92d2efe214ca9deeacf4575d052502c9
James Monger <jameskmonger@hotmail.co.uk>
2015-12-17 23:58:55 +0000

    Ugly and inefficient but it works

--- day_17/js/1.js
+++ day_17/js/1.js
@@ -6,6 +6,19 @@ var sizes = `20
 5
 5`.split('\n').map(s => parseInt(s));
 
+var jugs = [];
+
+class Size {
+  constructor(index, size) {
+    this.index = index;
+    this.size = size;
+  }
+}
+
+for (let s in sizes) {
+  jugs.push(new Size(s, sizes[s]));
+}
+
 const REQUIRED_AMOUNT = 25;
 
 var results = [];
@@ -15,15 +28,17 @@ var i = 0;
 while (true) {
   i++;
 
-  if (i > 50000)
-  break;
+  if (i > 5000000)
+    break;
 
-  var shuf = getRandom(sizes, Math.floor(Math.random() * sizes.length) + 1).sort();
+  var shuf = getRandom(jugs, Math.floor(Math.random() * jugs.length) + 1).sort(function (a, b) {
+    return a.index - b.index;
+  });
 
   if (sum(shuf) === REQUIRED_AMOUNT) {
     var str = "";
     for (var s in shuf) {
-      str += shuf[s] + ",";
+      str += shuf[s].index + ",";
     }
 
     if (results.indexOf(str) === -1) {
@@ -32,24 +47,40 @@ while (true) {
   }
 }
 
-console.log(results);
+console.log(results.length);
 
 function sum (arr) {
   var count = 0;
-  arr.forEach(x => { count += x; });
+  arr.forEach(j => { count += j.size; });
   return count;
 }
 
+function shuffle(array) {
+  var counter = array.length, temp, index;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
 function getRandom (arr, n) {
-    var result = new Array(n),
-        len = arr.length,
-        taken = new Array(len);
-    if (n > len)
-        throw new RangeError("getRandom: more elements taken than available");
-    while (n--) {
-        var x = Math.floor(Math.random() * len);
-        result[n] = arr[x in taken ? taken[x] : x];
-        taken[x] = --len;
-    }
-    return result;
+  arr = shuffle(arr);
+
+  let out = [];
+  for (var i = 0; i < n; i++) {
+    out.push(arr[i]);
+  }
+  return out;
 }
