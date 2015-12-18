485d98f9e127e89d3d87582afbb563a0f867ae90
James Monger <jameskmonger@hotmail.co.uk>
2015-12-18 09:35:25 +0000

    Parse the input

--- day_18/js/1.js
+++ day_18/js/1.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const CHARACTER_ON = '#';
+const CHARACTER_OFF = '.';
+
 let inputLines = `.#.#.#
 ...##.
 #....#
@@ -11,6 +14,16 @@ let lights = [];
 
 for (let i = 0; i < inputLines.length; i++) {
   lights[i] = [];
+
+  for (let c = 0; c < inputLines[i].length; c++) {
+    let char = inputLines[i][c];
+
+    if (char === CHARACTER_ON) {
+      lights[i][c] = true;
+    } else if (char === CHARACTER_OFF) {
+      lights[i][c] = false;
+    }
+  }
 }
 
 console.log(lights);
