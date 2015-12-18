3798cd6b257c6acff84b2c837785b32e083c159b
James Monger <jameskmonger@hotmail.co.uk>
2015-12-18 09:32:30 +0000

    Started day 18 part 1 JS solution

---
+++ day_18/js/1.js
@@ -0,0 +1,14 @@
+let inputLines = `.#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####..`.split('\n');
+
+let lights = [];
+
+for (let i = 0; i < inputLines.length; i++) {
+  lights[i] = [];
+}
+
+console.log(lights);
