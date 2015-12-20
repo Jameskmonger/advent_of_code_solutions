5b44a17c6f97bb5e34bedd0861e053f623d5e4d1
James Monger <jameskmonger@hotmail.co.uk>
2015-12-20 22:53:40 +0000

    Create the different replacements

--- day_19/js/1.js
+++ day_19/js/1.js
@@ -1,10 +1,31 @@
 "use strict";
 
-let REPLACEMENTS = {
-  "H": ["HO", "OH"],
-  "O": ["HH"]
-};
+let REPLACEMENTS = [
+  ["H", "HO"],
+  ["H", "OH"],
+  ["O", "HH"]
+];
 
-for (let r in REPLACEMENTS) {
-  console.log(REPLACEMENTS[r]);
+let INPUT = "HOH";
+
+let output = [];
+
+for (let r of REPLACEMENTS) {
+  let position = 0;
+  while (true) {
+    position = INPUT.indexOf(r[0], position);
+
+    if (position === -1) {
+      break;
+    }
+
+    output.push(replace(INPUT, r[0], r[1], position));
+    position += r[0].length;
+  }
+}
+
+console.log(output);
+
+function replace(s, i, o, pos) {
+  return s.substring(0, pos) + o + s.substring(pos + i.length);
 }
