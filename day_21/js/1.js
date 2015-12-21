84092f312078b87ab64b8dcd0e4daeb0e5ff3108
James Monger <jameskmonger@hotmail.co.uk>
2015-12-21 10:10:22 +0000

    Add item definitions

--- day_21/js/1.js
+++ day_21/js/1.js
@@ -1,11 +1,44 @@
 (function() {
   "use strict";
 
-  class Stats {
-    constructor(hitpoints, armor, damage) {
-      this.hitpoints = hitpoints;
-      this.armor = armor;
+  class Item {
+    constructor(cost, damage, armor) {
+      this.cost = cost;
       this.damage = damage;
+      this.armor = armor;
+    }
+  }
+
+  let WEAPONS = [
+    new Item(8, 4, 0),
+    new Item(10, 5, 0),
+    new Item(25, 6, 0),
+    new Item(40, 7, 0),
+    new Item(74, 8, 0)
+  ];
+
+  let ARMORS = [
+    new Item(13, 0, 1),
+    new Item(31, 0, 2),
+    new Item(53, 0, 3),
+    new Item(75, 0, 4),
+    new Item(102, 0, 5)
+  ];
+
+  let RINGS = [
+    new Item(25, 1, 0),
+    new Item(50, 2, 0),
+    new Item(100, 3, 0),
+    new Item(20, 0, 1),
+    new Item(40, 0, 2),
+    new Item(80, 0, 3)
+  ];
+
+  class Stats {
+    constructor(hitpoints, damage, armor) {
+      this.hitpoints = hitpoints;
+      this.damage = damage;
+      this.armor = armor;
     }
 
     attack(other) {
@@ -22,7 +55,7 @@
   }
 
   let player = new Stats(8, 5, 5);
-  let boss = new Stats(12, 2, 7);
+  let boss = new Stats(12, 7, 2);
 
   player.attack(boss);
   boss.attack(player);
