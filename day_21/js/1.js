ac4bffe51e1def68eac11116e0e83bdf40ac5a5f
James Monger <jameskmonger@hotmail.co.uk>
2015-12-21 10:30:27 +0000

    Random outfit generation

--- day_21/js/1.js
+++ day_21/js/1.js
@@ -54,14 +54,55 @@
     }
   }
 
+  function getRandomOutfit() {
+    let weapon = WEAPONS[Math.floor(Math.random() * WEAPONS.length)];
+    let armor = ARMORS[Math.floor(Math.random() * ARMORS.length)];
+
+    let ringCount = getRandomInt(0, 2);
+    let rings = [];
+
+    if (ringCount === 0) {
+      rings = [];
+    } else if (ringCount === 1) {
+      rings = [RINGS[Math.floor(Math.random() * RINGS.length)]];
+    } else if (ringCount === 2) {
+      let temp = RINGS.slice(0);
+
+      let first = temp[Math.floor(Math.random() * temp.length)];
+      rings.push(first);
+      temp.splice(temp.indexOf(first), 1);
+
+      let second = temp[Math.floor(Math.random() * temp.length)];
+      rings.push(second);
+      temp.splice(temp.indexOf(second), 1);
+    }
+
+    let total_cost = 0, total_dmg = 0, total_arm = 0;
+    total_cost += weapon.cost;
+    total_dmg += weapon.damage;
+    total_arm += weapon.armor;
+
+    total_cost += armor.cost;
+    total_dmg += armor.damage;
+    total_arm += armor.armor;
+
+    for (let r of rings) {
+      total_cost += r.cost;
+      total_dmg += r.damage;
+      total_arm += r.armor;
+    }
+
+    return new Item(total_cost, total_dmg, total_arm);
+  }
+
   let player = new Stats(8, 5, 5);
   let boss = new Stats(12, 7, 2);
 
-  player.attack(boss);
-  boss.attack(player);
-  player.attack(boss);
-  boss.attack(player);
-  player.attack(boss);
-  boss.attack(player);
-  player.attack(boss);
+  for (var i = 0; i < 10; i++) {
+    console.log(getRandomOutfit());
+  }
 })();
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
