0f813c5708b1805a767327cf16282d5237a62a31
James Monger <jameskmonger@hotmail.co.uk>
2015-12-21 00:21:43 +0000

    Finish solution for part 1

---
+++ day_20/js/1.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let houses = [];
+let ELF_COUNT = 2147483647;
+let HOUSE_COUNT = 10000000;
+
+for (let e = 0; e < ELF_COUNT; e++) {
+  for (let h = (e + 1); h < HOUSE_COUNT + 1; h += (e + 1)) {
+    if (houses[h] === undefined) {
+      houses[h] = 0;
+    }
+
+    houses[h] += ((e + 1) * 10);
+
+    if (houses[h] > 29000000) {
+      console.log(h);
+      return;
+    }
+  }
+}
