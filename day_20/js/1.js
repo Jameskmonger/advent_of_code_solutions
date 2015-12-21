a5a39666ed27d6b41d4a4a95fcf9c3d3ee71cd63
James Monger <jameskmonger@hotmail.co.uk>
2015-12-21 00:25:50 +0000

    Try to fix solution for part 1

--- day_20/js/1.js
+++ day_20/js/1.js
@@ -2,7 +2,7 @@
 
 let houses = [];
 let ELF_COUNT = 2147483647;
-let HOUSE_COUNT = 10000000;
+let HOUSE_COUNT = 9424799;
 
 for (let e = 0; e < ELF_COUNT; e++) {
   for (let h = (e + 1); h < HOUSE_COUNT + 1; h += (e + 1)) {
