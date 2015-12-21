88a77cb475c82b5d823051dae98b1475a7400378
James Monger <jameskmonger@hotmail.co.uk>
2015-12-21 00:33:04 +0000

    Finish solution part 1

--- day_20/js/1.js
+++ day_20/js/1.js
@@ -2,10 +2,20 @@
 
 let houses = [];
 let ELF_COUNT = 2147483647;
-let HOUSE_COUNT = 9424799;
+let HOUSE_COUNT = 8648640;
+
+let first_house = HOUSE_COUNT;
 
 for (let e = 0; e < ELF_COUNT; e++) {
-  for (let h = (e + 1); h < HOUSE_COUNT + 1; h += (e + 1)) {
+  if (e % 5000 === 0) {
+    console.log("starting elf " + e);
+  }
+
+  if (e > first_house) {
+    break;
+  }
+
+  for (let h = (e + 1); h < first_house + 1; h += (e + 1)) {
     if (houses[h] === undefined) {
       houses[h] = 0;
     }
@@ -13,8 +23,13 @@ for (let e = 0; e < ELF_COUNT; e++) {
     houses[h] += ((e + 1) * 10);
 
     if (houses[h] > 29000000) {
-      console.log(h);
-      return;
+      if (h < first_house) {
+        first_house = h;
+      }
+
+      break;
     }
   }
 }
+
+console.log(first_house);
