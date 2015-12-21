dcb42b6490e2f31f96a9c328973190e4d2165b03
James Monger <jameskmonger@hotmail.co.uk>
2015-12-21 10:01:37 +0000

    Example recreated

--- day_21/js/1.js
+++ day_21/js/1.js
@@ -7,8 +7,28 @@
       this.armor = armor;
       this.damage = damage;
     }
+
+    attack(other) {
+      let damage = (this.damage - other.armor);
+
+      if (damage < 1) {
+        damage = 1;
+      }
+
+      other.hitpoints -= damage;
+
+      console.log(other);
+    }
   }
 
   let player = new Stats(8, 5, 5);
-  let boss = new Stats(12, 7, 2);
+  let boss = new Stats(12, 2, 7);
+
+  player.attack(boss);
+  boss.attack(player);
+  player.attack(boss);
+  boss.attack(player);
+  player.attack(boss);
+  boss.attack(player);
+  player.attack(boss);
 })();
