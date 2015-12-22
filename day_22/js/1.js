d3c1c524c2e8b637c0c9b72966c578671d99a34d
James Monger <jameskmonger@hotmail.co.uk>
2015-12-22 13:42:45 +0000

    Solution complete

--- day_22/js/1.js
+++ day_22/js/1.js
@@ -21,97 +21,118 @@
     new Spell("Recharge", 229, 0, 0, 0, 101, 5)
   ];
 
-  let spells = [];
-  let bossHealth = 51;
-  let bossDamage = 9;
-  let myHealth = 50;
-  let myMana = 500;
+  let least_spent = 9999999;
 
-  function getEffects() {
-    if (spells.length === 0) {
+  for (var i = 0; i < 5000000; i++) {
+    let spells = [];
+    let bossHealth = 51;
+    let bossDamage = 9;
+    let myHealth = 50;
+    let myMana = 500;
+    let spent = 0;
+
+    function getEffects() {
+      if (spells.length === 0) {
+        return {
+          mana: 0,
+          damage: 0,
+          armor: 0
+        };
+      }
+
+      let mana = spells.map(s => s.mana).reduce((a, b) => a + b);
+      let damage = spells.map(s => s.damage).reduce((a, b) => a + b);
+      let armor = spells.map(s => s.armor).reduce((a, b) => a + b);
+
       return {
-        mana: 0,
-        damage: 0,
-        armor: 0
+        mana: mana,
+        damage: damage,
+        armor: armor
       };
     }
 
-    let mana = spells.map(s => s.mana).reduce((a, b) => a + b);
-    let damage = spells.map(s => s.damage).reduce((a, b) => a + b);
-    let armor = spells.map(s => s.armor).reduce((a, b) => a + b);
-
-    return {
-      mana: mana,
-      damage: damage,
-      armor: armor
-    };
-  }
-
-  function processSpellDurations() {
-    spells = spells.map(s => {
-      s.duration -= 1;
-      return s;
-    }).filter(s => s.duration > 0);
-  }
-
-  function getAvailableSpells() {
-    // Return the spells where the cost is less than or equal to our mana
-    // And where there are 0 spells with a matching name currently in play
-    return SPELLS.filter(s => s.cost <= myMana).filter(s => {
-      return (spells.filter(o => o.name === s.name).length === 0);
-    });
-  }
-
-  function play(myTurn) {
-    let effects = getEffects();
-
-    bossHealth -= effects.damage;
-
-    if (bossHealth <= 0) {
-      console.log("boss has died");
-      return;
+    function processSpellDurations() {
+      spells = spells.map(s => {
+        s.duration -= 1;
+        return s;
+      }).filter(s => s.duration > 0);
     }
 
-    processSpellDurations();
+    function getAvailableSpells() {
+      // Return the spells where the cost is less than or equal to our mana
+      // And where there are 0 spells with a matching name currently in play
+      return SPELLS.filter(s => s.cost <= myMana).filter(s => {
+        return (spells.filter(o => o.name === s.name).length === 0);
+      });
+    }
 
-    if (myTurn) {
-      let available = getAvailableSpells();
+    function play(myTurn) {
+      let effects = getEffects();
 
-      if (available.length === 0) {
-        console.log("no more spells to cast");
+      bossHealth -= effects.damage;
+      myMana += effects.mana;
+
+      if (bossHealth <= 0) {
+        console.log("boss has died (" + i + ")");
+
+        if (spent < least_spent) {
+          least_spent = spent;
+        }
+
         return;
       }
 
-      let random = available[Math.floor(Math.random() * available.length)];
+      processSpellDurations();
 
-      myMana -= random.cost;
+      if (myTurn) {
+        let available = getAvailableSpells();
 
-      if (random.duration === 1) {
-        myHealth += random.heal;
-        bossDamage += random.damage;
-
-        if (bossHealth <= 0) {
-          console.log("boss has died");
+        if (available.length === 0) {
           return;
         }
+
+        let random = available[Math.floor(Math.random() * available.length)];
+
+        myMana -= random.cost;
+        spent += random.cost;
+
+        if (random.duration === 1) {
+          myHealth += random.heal;
+          bossHealth -= random.damage;
+
+          if (bossHealth <= 0) {
+            console.log("boss has died (" + i + ")");
+
+            if (spent < least_spent) {
+              least_spent = spent;
+            }
+
+            return;
+          }
+        } else {
+          spells.push(JSON.parse(JSON.stringify(random)));
+        }
+
+        if (spent < least_spent) {
+          play(false);
+        }
       } else {
-        spells.push(JSON.parse(JSON.stringify(random)));
+        let damage = Math.max((bossDamage - effects.armor), 1);
+        myHealth -= damage;
+
+        if (myHealth <= 0) {
+          return;
+        }
+
+        if (spent < least_spent) {
+          play(true);
+        }
       }
-
-      play(false);
-    } else {
-      let damage = Math.max((bossDamage - effects.armor), 1);
-      myHealth -= damage;
-
-      if (myHealth <= 0) {
-        console.log("player has died");
-        return;
-      }
-
-      play(true);
     }
+
+    play(true);
   }
 
-  play(true);
+  console.log("lowest spend: " + least_spent);
 
 })();
