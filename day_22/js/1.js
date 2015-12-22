c2513bedf9f6c38781dc4a154c90783bb4b60c0e
James Monger <jameskmonger@hotmail.co.uk>
2015-12-22 09:39:03 +0000

    Set up Wizard and Warrior classes

--- day_22/js/1.js
+++ day_22/js/1.js
@@ -8,16 +8,54 @@
     }
 
     attack(me, other) {
-      this.func(other);
+      this.func(me, other);
+    }
+  }
+
+  class Wizard {
+    constructor(hitpoints, mana) {
+      this.hitpoints = hitpoints;
+      this.mana = mana;
+    }
+
+    hit(count) {
+      this.hitpoints -= count;
+    }
+
+    heal(count) {
+      this.hitpoints += count;
+    }
+  }
+
+  class Warrior {
+    constructor(hitpoints, damage) {
+      this.hitpoints = hitpoints;
+      this.damage = damage;
+    }
+
+    hit(count) {
+      this.hitpoints -= count;
+    }
+
+    heal(count) {
+      this.hitpoints += count;
     }
   }
 
   const MAGIC_MISSILE = new Spell(function(me, other) {
-    other.damage(4);
+    other.hit(4);
   }, 53);
 
   const DRAIN = new Spell(function(me, other) {
-    other.damage(2);
+    other.hit(2);
     me.heal(2);
   }, 73);
+
+  let Wiz = new Wizard(12, 25);
+  let War = new Warrior(12, 5);
+
+  MAGIC_MISSILE.attack(Wiz, War);
+
+  console.log(Wiz);
+  console.log(War);
 })();
