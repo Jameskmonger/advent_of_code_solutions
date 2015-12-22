5ef9ef8b920452ff20cc0daaa47da0550b71fe48
James Monger <jameskmonger@hotmail.co.uk>
2015-12-22 12:53:40 +0000

    Define spells in an array

--- day_22/js/1.js
+++ day_22/js/1.js
@@ -2,120 +2,25 @@
   "use strict";
 
   class Spell {
-    constructor(func, cost) {
-      this.func = func;
+    constructor(name, cost, damage, heal, armor, mana, duration) {
+      this.name = name;
       this.cost = cost;
-    }
-
-    attack(me, other) {
-      this.func(me, other);
-    }
-  }
-
-  class Effect {
-    constructor(func) {
-      this.func = func;
-    }
-
-    apply(target) {
-      this.func(target);
-    }
-  }
-
-  class Wizard {
-    constructor(hitpoints, mana) {
-      this.hitpoints = hitpoints;
-      this.mana = mana;
-      this.armor = 0;
-
-      this.effects = [];
-    }
-
-    hit(count) {
-      this.hitpoints -= count;
-    }
-
-    heal(count) {
-      this.hitpoints += count;
-    }
-
-    takeTurn(target) {
-      if (this.effects.shield !== undefined && this.effects.shield >= 0) {
-        SHIELD_EFFECT.apply(this);
-        this.effects.shield--;
-      }
-
-      if (this.effects.poison !== undefined && this.effects.poison >= 0) {
-        POISON_EFFECT.apply(target);
-        this.effects.poison--;
-      }
-
-      if (this.effects.recharge !== undefined && this.effects.recharge >= 0) {
-        RECHARGE_EFFECT.apply(this);
-        this.effects.recharge--;
-      }
-    }
-  }
-
-  class Warrior {
-    constructor(hitpoints, damage) {
-      this.hitpoints = hitpoints;
       this.damage = damage;
-      this.armor = 0;
-    }
-
-    hit(count) {
-      this.hitpoints -= count;
-    }
-
-    heal(count) {
-      this.hitpoints += count;
-    }
-
-    takeTurn(target) {
-      target.hit(this.damage - target.armor);
+      this.heal = heal;
+      this.armor = armor;
+      this.mana = mana;
+      this.duration = duration;
     }
   }
 
-  const MAGIC_MISSILE = new Spell(function(me, other) {
-    other.hit(4);
-  }, 53);
+  const SPELLS = [
+    new Spell("Magic Missile", 53, 4, 0, 0, 0, 1),
+    new Spell("Drain", 73, 2, 2, 0, 0, 1),
+    new Spell("Shield", 113, 0, 0, 7, 0, 6),
+    new Spell("Poison", 173, 3, 0, 0, 0, 6),
+    new Spell("Recharge", 229, 0, 0, 0, 101, 5)
+  ];
 
-  const DRAIN = new Spell(function(me, other) {
-    other.hit(2);
-    me.heal(2);
-  }, 73);
+  console.log(SPELLS);
 
-  const SHIELD_EFFECT = new Effect(function(target) {
-    target.armor = 7;
-  });
-
-  const SHIELD = new Spell(function(me, other) {
-    if (me.effects.shield === 0 || me.effects.shield === undefined) {
-      me.effects.shield = 6;
-    }
-  }, 113);
-
-  const POISON_EFFECT = new Effect(function(target) {
-    target.hit(3);
-  });
-
-  const POISON = new Spell(function(me, other) {
-    if (me.effects.poison === 0 || me.effects.poison === undefined) {
-      me.effects.poison = 6;
-    }
-  }, 173);
-
-  const RECHARGE_EFFECT = new Effect(function(target) {
-    target.mana += 101;
-  });
-
-  const RECHARGE = new Spell(function(me, other) {
-    if (me.effects.recharge === 0 || me.effects.recharge === undefined) {
-      me.effects.recharge = 5;
-    }
-  }, 229);
-
-  let player = new Wizard(10, 250);
-  let boss = new Warrior(13, 8);
 })();
