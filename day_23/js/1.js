21e1daa9e158002be9d250b15d634d54fed5904c
James Monger <jameskmonger@hotmail.co.uk>
2015-12-23 09:21:00 +0000

    Added triple instruction

--- day_23/js/1.js
+++ day_23/js/1.js
@@ -8,6 +8,11 @@ inc a`.split('\n');
 
   let registers = [];
 
+  let pointer = 0;
+  while (true) {
+
+  }
+
   function processInstruction(instruction) {
     let parts = instruction.split(' ');
     let tag = parts[0];
@@ -16,6 +21,10 @@ inc a`.split('\n');
       case "hlf":
         let reg = parts[1];
         registers[reg] /= 2;
+        break;
+      case "tpl":
+        let reg = parts[1];
+        registers[reg] *= 3;
         break;
     }
   }
