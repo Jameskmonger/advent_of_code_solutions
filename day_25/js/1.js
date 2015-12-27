5d12ad5ee721dafcfdf2ea80e6347c4d4192aaa4
James Monger <jameskmonger@hotmail.co.uk>
2015-12-27 23:40:10 +0000

    Solution for day 25 part 1 complete

--- day_25/js/1.js
+++ day_25/js/1.js
@@ -1,3 +1,28 @@
 (function() {
-    'use strict';
+  'use strict';
+
+  let current_x = 1;
+  let current_y = 1;
+
+  let current_val = 20151125;
+
+  let input_x = 3019;
+  let input_y = 3010;
+
+  while (true) {
+    if (current_y === 1) {
+      current_y = current_x + 1;
+      current_x = 1;
+    } else {
+      current_y -= 1;
+      current_x += 1;
+    }
+
+    current_val = (current_val * 252533) % 33554393;
+
+    if (current_x === input_x && current_y === input_y) {
+      console.log(current_val);
+      break;
+    }
+  }
 })();
