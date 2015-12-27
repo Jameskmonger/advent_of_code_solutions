da2c4d99aafe26cad2632385b9568733305c60db
James Monger <jameskmonger@hotmail.co.uk>
2015-12-27 23:41:05 +0000

    Refactored 'current' data

--- day_25/js/1.js
+++ day_25/js/1.js
@@ -1,27 +1,28 @@
 (function() {
   'use strict';
 
-  let current_x = 1;
-  let current_y = 1;
-
-  let current_val = 20151125;
+  let current = {
+    x: 1,
+    y: 1,
+    val: 20151125
+  };
 
   let input_x = 3019;
   let input_y = 3010;
 
   while (true) {
-    if (current_y === 1) {
-      current_y = current_x + 1;
-      current_x = 1;
+    if (current.y === 1) {
+      current.y = current.x + 1;
+      current.x = 1;
     } else {
-      current_y -= 1;
-      current_x += 1;
+      current.y -= 1;
+      current.y += 1;
     }
 
-    current_val = (current_val * 252533) % 33554393;
+    current.val = (current.val * 252533) % 33554393;
 
-    if (current_x === input_x && current_y === input_y) {
-      console.log(current_val);
+    if (current.x === input_x && current.y === input_y) {
+      console.log(current.val);
       break;
     }
   }
